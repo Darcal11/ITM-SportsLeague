041d576a6c160e932084f3ac1d34c51ccf886817
Darcal11 <darwincalderon675@gmail.com>
2026-05-17 12:12:21 -0500

    Phase 5.1: DataSeeder created

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using SportsLeague.DataAccess.Context;
 using SportsLeague.DataAccess.Repositories;
+using SportsLeague.DataAccess.Seeders;
 using SportsLeague.Domain.Helpers;
 using SportsLeague.Domain.Interfaces.Repositories;
 using SportsLeague.Domain.Interfaces.Services;
 using SportsLeague.Domain.Services;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -58,6 +58,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
+// ── Data Seeder ── 
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider
+        .GetRequiredService<LeagueDbContext>();
+
+    await context.Database.MigrateAsync(); // Crea la BD + aplica migraciones 
+    await DataSeeder.SeedAsync(context);
+}
 
 // ── Middleware Pipeline ──
 
